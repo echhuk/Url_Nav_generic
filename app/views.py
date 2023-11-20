e25bbd2edf48cc5e521e76279c21d4bb23e7fcc4
echhuk <echhuksharma@gmail.com>
2023-11-20 23:15:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def echhuk(request):
+    return render(request,'echhuk.html')
+
+def jspider(request):
+    return render(request,'jspider.html')
+
+def ask(request):
+    return render(request,'ask.html')
